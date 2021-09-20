7bb2b1de8fe01b30ae1948afc8191f95961ac6ed
Atid khodaie <amirmahdijafarian@gmail.com>
2021-09-20 18:56:20 +0430

    Add models and do some simple settings in models.py also some special settings in admin.py Uploading images are fixed.

--- food/admin.py
+++ food/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import  foodModels
 
+@admin.register(foodModels)
+class adminFoodModels(admin.ModelAdmin):
+    list_display = ('name', 'rate','auth', 'check')
+    search_fields = ('desc', 'name')
+    prepopulated_fields = {'slug':('name',)}
+    list_filter = ('check',)
 # Register your models here.
--- food/models.py
+++ food/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
+class foodModels(models.Model):
+    RATE = (
+        ('3','Very good'),
+        ('2', 'Good'),
+        ('1','Bad'),
+    )
+    AUTH = (
+        ('Admins',(
+            ('A','Atid'),
+            ('R','Rahmat'),
+            ('N','Noora'),
+        )),
+        ('Normal people',(
+            ('Y','Yasin'),
+            ('E','Ebne')
+        )),
+
+    )
+    name = models.CharField(max_length=20)
+    slug = models.SlugField(max_length=20)
+    desc = models.TextField(default="add descriptions here.")
+    rate = models.CharField(max_length=1, choices=RATE)
+    auth = models.CharField(max_length=1, choices=AUTH)  # special feature
+    img = models.ImageField(upload_to='foodPics/')
+    check = models.BooleanField(default=False, verbose_name="آیا اجازه انتشار می‌دهید؟")
+
+
+    class Meta:
+        verbose_name = 'غذا'
+        verbose_name_plural = 'غذاها'
+
+    def __str__(self):
+        return self.name
 # Create your models here.
