17cf97797276e30655d5983b8706f67f94f093ed
Atid khodaie <amirmahdijafarian@gmail.com>
2021-09-21 02:29:39 +0430

    add ListView. - by adding ListView I got a but which removed categories from navbar.

--- food/urls.py
+++ food/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import homeView, detailShow, categoryShow
+from .views import  detailShow, categoryShow, base
 
 app_name = 'foodUrl'
 
 urlpatterns = [
-    path('', homeView, name='home'),
+    path('',base.as_view() , name='home'),
     path('<slug:theSLUG>',detailShow ),
     path('articles/<slug:categID>', categoryShow)
 ]
--- food/views.py
+++ food/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
 from .models import foodModels, Category
 from django.core.paginator import Paginator
+from django.views.generic import ListView
+#
+# def homeView(request):
+#     ArticleList = foodModels.objects.show()
+#     paginator = Paginator(ArticleList, 3)
+#     page = request.GET.get('page')
+#     pageCONT = paginator.get_page(page)
+#     context = {
+#         'foods': pageCONT,
+#     }
+#     return render(request, 'index.html', context)
+class base(ListView):
+    template_name = 'index.html'
+    queryset = foodModels.objects.show()
+    paginate_by = 3
+    context_object_name = 'foods'
 
-def homeView(request):
-    ArticleList = foodModels.objects.show()
-    paginator = Paginator(ArticleList, 3)
-    page = request.GET.get('page')
-    pageCONT = paginator.get_page(page)
-    context = {
-        'foods': pageCONT,
-        'cat': Category.objects.all()
-    }
-    return render(request, 'index.html', context)
 def detailShow(request, theSLUG):
     context = {
         'details': foodModels.objects.get(slug=theSLUG)
